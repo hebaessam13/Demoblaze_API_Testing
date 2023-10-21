f62330652fc68d95191635f2a145f9a15c0257fc
hebaessam13 <107505770+hebaessam13@users.noreply.github.com>
2023-10-21 16:15:59 +0300

    updating reports and logs

--- test_data/users_test_data.py
+++ test_data/users_test_data.py
@@ -9,10 +9,10 @@ SIGNUP_TEST_DATA = [('keirahahe', '123', 400, 'This user already exist.'),
                     (data_generator.generate_string(10), data_generator.generate_string(10), 200, '')]
 
 LOGIN_TEST_DATA = [('keirahahe', '123', 200, ''),
-                   ('keirahahe', 'xyz', 401, 'Wrong password'),
-                   ('keira', '123', 401, 'User does not exist'),
-                   ('thisauserthatdoesntexist', '12345', 401, 'does not exist'),
+                   ('keirahahe', 'xyz', 401, 'Wrong user name or password'),
+                   ('keira', '123', 401, 'wrong username or password'),
+                   ('thisauserthatdoesntexist', '12345', 401, 'wrong username or password'),
                    ('', '12345', 400, 'username cant be empty'),
                    ('thisauserthatdoesntexist', '', 400, 'password cant be empty')]
 
-INVALID_API_METHODS = ['PUT', 'PATCH', 'GET', 'DELETE']
+INVALID_API_METHODS = ['PUT', 'PATCH']
